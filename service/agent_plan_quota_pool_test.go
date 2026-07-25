e221fb95a0064bc4cee7d713353404dee33863de
wxdl521 <1619804403@qq.com>
2026-07-25 12:00:58 +0800

    test: update agent plan imports for relaykit

--- service/agent_plan_quota_pool_test.go
+++ service/agent_plan_quota_pool_test.go
@@ -11,9 +11,9 @@ import (
 
 	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/constant"
-	"github.com/QuantumNous/new-api/dto"
 	"github.com/QuantumNous/new-api/model"
 	relaycommon "github.com/QuantumNous/new-api/relay/common"
+	"github.com/QuantumNous/new-api/relaykit/dto"
 	"github.com/gin-gonic/gin"
 	"github.com/glebarez/sqlite"
 	"github.com/stretchr/testify/assert"
